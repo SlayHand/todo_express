b20db4cb3bcb1356db15eeac2ae8f975d47569fc
fredy <fredy-andri.mottus@voco.ee>
2024-10-29 14:24:04 +0200

    create project basic files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.send(path.join (__dirname, 'views'))
+})
+
+app.listen(3001, () => {
+    console.log('Server started at http://localhost:3001')
+})
