d95fad82a99566f98ab34139a32dcb35e518ae19
fredy <fredy-andri.mottus@voco.ee>
2024-10-29 14:59:02 +0200

    korrastatud

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.send(path.join (__dirname, 'views'))
+    res.render('index')
 })
 
 app.listen(3001, () => {
