f551c78944e00646308dae158f984d2de742c2da
fredy <fredy-andri.mottus@voco.ee>
2024-10-31 13:09:23 +0200

    Read data from file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('node:fs')
 
 const app = express()
 
@@ -7,8 +8,14 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    const tasks =['Study JS', 'Study Templating', 'Study HTTP'] 
-    res.render('index', {tasks: tasks})
+    fs.readFile('./tasks', 'utf-8', (error, data) => {
+        if (error) {
+            console.error(error)
+            return
+        } 
+        const tasks = data.split('\n')
+        res.render('index', {tasks: tasks})
+    })
 })
 
 app.listen(3001, () => {
