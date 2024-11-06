7dbd63d832b9a738f092ab1b4355ee42314d1e27
fredy <fredy-andri.mottus@voco.ee>
2024-11-06 13:53:46 +0200

    Delete single task data from file

--- index.js
+++ index.js
@@ -59,6 +59,25 @@ app.post('/', (req, res) =>{
         })
     })
 })
+app.get('/delete-task/:taskId', (reg,res) =>{
+    let deletedTaskId = parseInt (req.params.taskId)
+    readFile('./tasks.json')
+    .then(tasks => {
+        tasks.forEach((task, index) => {
+            if(task.id === deletedTaskId){
+                tasks.splice(index, 1)
+            } 
+        })
+        data = JSON.stringify(tasks, null, 2)
+        fs.writeFile('./tasks.json', data, 'utf-8', err => {
+            if (err){
+                console.error(err);
+                return;
+            }
+            res.redirect('/') 
+        })
+    })
+})
 
 app.listen(3001, () => {
     console.log('Server started at http://localhost:3001')
